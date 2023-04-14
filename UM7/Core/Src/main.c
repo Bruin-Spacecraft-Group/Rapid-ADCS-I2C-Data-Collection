f82692150d1875999498281ca0cd3e01b45eba29
Touwfoo <jlee.ian42@gmail.com>
2023-04-13 19:27:10 -0700

    created a thing

--- UM7/Core/Src/main.c
+++ UM7/Core/Src/main.c
@@ -40,8 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-SPI_HandleTypeDef hspi1;
-
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -52,13 +51,13 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_SPI1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 void UART_PRINT_VAL(double value);
 void UART_PRINT_TEXT(uint8_t* MSG);
 
 void UM7_INIT(void);
-double UM7_GET_DATA(uint8_t addr);
+uint32_t UM7_GET_DATA(uint8_t addr);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -95,7 +94,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_SPI1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   	double xAccel = 0;
 	double yAccel = 0;
@@ -118,17 +117,25 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  xAccel = UM7_GET_DATA(0x65);
+	  //xAccel = UM7_GET_DATA(0x65);
+	  //yAccel = UM7_GET_DATA(0X66);
+	  //zAccel = UM7_GET_DATA(0x67);
 
-	  UART_PRINT_TEXT("Accelerometer: ");
-	  UART_PRINT_TEXT("( ");
-	  UART_PRINT_VAL(xAccel);
+	  uint8_t rx_data[11] = {0};
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+	  HAL_UART_Receive(&huart1, rx_data, 11, 50);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+	  UART_PRINT_TEXT(rx_data);
+	  UART_PRINT_TEXT("\n");
+	  //UART_PRINT_TEXT("Accelerometer: ");
+	  //UART_PRINT_TEXT("( ");
+	  //UART_PRINT_VAL(xAccel);
+	  //UART_PRINT_TEXT(", ");
+	  //UART_PRINT_VAL(yAccel);
+	  //UART_PRINT_TEXT(", ");
+	  //UART_PRINT_VAL(zAccel);
+	  //UART_PRINT_TEXT(" )\n");
 	  /*
-	  UART_PRINT_TEXT(", ");
-	  UART_PRINT_VAL(yAccel);
-	  UART_PRINT_TEXT(", ");
-	  UART_PRINT_VAL(zAccel);
-	  UART_PRINT_TEXT(" )\n");
 	  UART_PRINT_TEXT("Magnetometer: ");
 	  UART_PRINT_TEXT("( ");
 	  UART_PRINT_VAL(xMag);
@@ -145,8 +152,8 @@ int main(void)
 	  UART_PRINT_TEXT(", ");
 	  UART_PRINT_VAL(zGyro);
 	  */
-	  UART_PRINT_TEXT(" )\n");
-	  HAL_Delay(150);
+	  //UART_PRINT_TEXT(" )\n");
+	  HAL_Delay(30);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -201,42 +208,50 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief SPI1 Initialization Function
+  * @brief USART1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_SPI1_Init(void)
+static void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN SPI1_Init 0 */
+  /* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END SPI1_Init 0 */
+  /* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN SPI1_Init 1 */
+  /* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END SPI1_Init 1 */
-  /* SPI1 parameter configuration*/
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 7;
-  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN SPI1_Init 2 */
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END SPI1_Init 2 */
+  /* USER CODE END USART1_Init 2 */
 
 }
 
@@ -328,13 +343,18 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+
+}
+
 void UART_PRINT_VAL(double value){
 	char total[50];
 	sprintf(total, "%i", (int)value);
 	strcat(total, ".");
 	char temp[10];
 	double currentVal = (value - (int) value);
-	for(int a=0;a<6;a++){
+	for(int a=0;a<1;a++){
 		currentVal *= 10;
 		sprintf(temp, "%i", abs((int)currentVal));
 		strcat(total, temp);
@@ -352,47 +372,69 @@ void UM7_INIT(void){
 	uint8_t tx_data3[4] = {0x00, 0x00, 0x00, 0xff};
 	UM7_WRITE(0xac, tx_data2); // reset to factory settings
 	HAL_Delay(1);
-	UM7_WRITE(0x02, tx_data2); // raw data rate (Hz)
-	UM7_WRITE(0x04, tx_data2); // processed data rate (Hz)
+	//UM7_WRITE(0x02, tx_data2); // raw data rate (Hz)
+	//UM7_WRITE(0x04, tx_data2); // processed data rate (Hz)
+	uint8_t tx_data4[4] = {0x00, 0x00, 0x00, 0x00};
+	UM7_WRITE(0x07, tx_data4);
+	HAL_Delay(5);
 }
+void UM7_WRITE(uint8_t addr, uint8_t data[4]){
+	uint8_t tx_data[11];
+	tx_data[0] = 's';
+	tx_data[1] = 'n';
+	tx_data[2] = 'p';
+	tx_data[3] = 0b10000000;
+	tx_data[4] = addr;
+	tx_data[5] = data[0];
+	tx_data[6] = data[1];
+	tx_data[7] = data[2];
+	tx_data[8] = data[3];
+	uint16_t sum = 0;
+	for(int a=0;a<9;a++){
+		sum += tx_data[a];
+	}
+	tx_data[9] = sum>>8;
+	tx_data[10] = sum;
+	HAL_UART_Transmit(&huart1, tx_data, 11, 100);
+}
+uint32_t UM7_GET_DATA(uint8_t addr){
+	uint16_t sum = 0;
+	sum = 's' + 'n' + 'p' + addr;
+	uint8_t front = sum>>8;
+	uint8_t back = sum;
+	uint8_t tx_data[11] = {'s', 'n', 'p', 0x00, addr, 0x00, 0x00, 0x00, 0x00, front, back};
+	uint8_t rx_data[11];
+	HAL_UART_Transmit(&huart1, tx_data, 11, 100);
+	HAL_UART_Receive(&huart1, rx_data, 11, 100);
+	return ((rx_data[5]<<24 | rx_data[6]<<16) | (rx_data[7]<<8 | rx_data[8]));
+}
+// SPI MODE STUFF COMMENTED OUT
+/*
 void UM7_WRITE(uint8_t addr, uint8_t data[4]){
 	uint8_t tx_data[6] = {0x01, addr, data[0], data[1], data[2], data[3]};
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 	HAL_SPI_Transmit(&hspi1, tx_data, 6, 100);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 }
-double power(double a, int b){
-	double temp = a;
-    if(b == 0){
-        return 1;
-    }
-    else{
-        for(int t=0;t<abs(b)-1;t++){
-            temp *= temp;
-        }
-    }
-    if(b < 1){
-        temp = 1.0 / temp;
-    }
-    return temp;
-}
 double UM7_GET_DATA(uint8_t addr){
 	double val = 0;
-	uint8_t tx_data[2];
-	uint8_t rx_data[4];
-	tx_data[0] = 0x00;
-	tx_data[1] = addr;
+	double value_exp = 0;
+	double value_frac = 0;
+	uint8_t tx_data[6] = {0x00, addr, 0x00, 0x00, 0x00, 0x00};
+	uint8_t rx_data[6];
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(&hspi1, tx_data, 2, HAL_MAX_DELAY);
-	HAL_SPI_Receive(&hspi1, rx_data, 4, HAL_MAX_DELAY);
+	//HAL_SPI_Transmit(&hspi1, tx_data, 2, 100);
+	//HAL_SPI_Receive(&hspi1, rx_data, 4, 100);
+	HAL_SPI_TransmitReceive(&hspi1, tx_data, rx_data, 6, HAL_MAX_DELAY);
+	//HAL_SPI_Receive_IT(&hspi1, rx_data, 4);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-	//value_exp = ((rx_data[0] & 0x7f) << 1) | (rx_data[1] >> 7);
-	//value_frac = ((rx_data[1] & 0x7f) << 16) | (rx_data[2] << 8 | rx_data[3]);
+	value_exp = ((rx_data[2] & 0x7f) << 1) | (rx_data[3] >> 7);
+	value_frac = ((rx_data[3] & 0x7f) << 16) | (rx_data[4] << 8 | rx_data[5]);
 	//calc1 = power(2.0,value_exp - 127);
-	//calc2 = rx_data[2];
-	val = rx_data[0];
+	val = (1.0 + value_frac / 10000000.0) * pow(2.0,value_exp - 127);
 	return val;
 }
+*/
 /* USER CODE END 4 */
 
 /**
